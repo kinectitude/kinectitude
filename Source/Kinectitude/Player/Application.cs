68c99e391fb15c1657ab4735662ace2fa25a2156
Brandon Walderman <bdwalder@uwaterloo.ca>
2012-05-09 15:56:49 -0400

    Changed the Render, Core, and Player projects to not copy SlimDX locally, and switched all SlimDX-related code to use DPIs instead of physical pixels.

--- Source/Kinectitude/Player/Application.cs
+++ Source/Kinectitude/Player/Application.cs
@@ -30,22 +30,24 @@ namespace Kinectitude.Player
 
         internal Application()
         {
+            drawFactory = new SlimDX.Direct2D.Factory();
+            SizeF dpi = drawFactory.DesktopDpi;
+
             GameLoader gameLoader = GameLoader.GetGameLoader("game.xml");
             game = gameLoader.Game;
 
-            Size size = new Size(game.Width, game.Height);
+            Size size = new Size( (int)(game.Width * dpi.Width / 96.0f), (int)(game.Height * dpi.Height / 96.0f) );
             form = new RenderForm(game.Name);
             form.ClientSize = size;
             form.FormBorderStyle = FormBorderStyle.FixedSingle;
             form.MaximizeBox = false;
 
-            drawFactory = new SlimDX.Direct2D.Factory();
             renderTarget = new WindowRenderTarget(drawFactory, new WindowRenderTargetProperties()
             {
                 Handle = form.Handle,
                 PixelSize = size
             });
-
+            
             writeFactory = new SlimDX.DirectWrite.Factory(SlimDX.DirectWrite.FactoryType.Shared);
             textFormat = writeFactory.CreateTextFormat("Arial", FontWeight.Regular, FontStyle.Normal, FontStretch.Normal, 36.0f, "en-us");
             textFormat.TextAlignment = TextAlignment.Center;
