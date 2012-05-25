ef0973d47b7713918522e244d5e415827024a8ad
Brandon Walderman <bdwalder@uwaterloo.ca>
2012-05-24 20:36:24 -0400

    Added requires and provides attributes.

---
+++ Source/Kinectitude/Attributes/RequiresAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kinectitude.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class RequiresAttribute : Attribute
+    {
+        private readonly Type type;
+
+        public RequiresAttribute(Type type)
+        {
+            this.type = type;
+        }
+    }
+}
