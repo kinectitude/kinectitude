0b80ea4ba752a234b6a30f3bd4ee8ca41307d4c4
unknown <rtinianov@uwaterloo.ca>
2012-06-04 11:01:07 -0400

    Changed the game.xml to work.  Sealed Entity.  Started a test for string data stuff

---
+++ Source/Kinectitude/Tests/Core/Data/StringDataTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kinectitude.Core.Data;
+
+namespace Kinectitude.Tests.Core.Data
+{
+    [TestClass]
+    public class StringDataTests
+    {
+        [TestMethod]
+        public void ConstantString()
+        {
+            ExpressionReader reader = ExpressionReader.CreateExpressionReader("constant", null, null);
+            Assert.IsTrue(reader.GetValue() == "constant");
+        }
+    }
+}
