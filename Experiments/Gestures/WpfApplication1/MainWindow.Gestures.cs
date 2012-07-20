612fbf95e33b3a9bb99bde7cede9f44e32842ff0
Scott <swreid@uwaterloo.ca>
2012-07-20 13:58:03 -0400

    Gesture experiments

---
+++ Experiments/Gestures/WpfApplication1/MainWindow.Gestures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using Kinect.Toolbox;
+using Microsoft.Kinect;
+
+namespace WpfApplication1
+{
+    partial class MainWindow
+    {
+        void LoadCircleGestureDetector()
+        {
+            using (Stream recordStream = File.Open(circleKBPath, FileMode.OpenOrCreate))
+            {
+                circleGestureRecognizer = new TemplatedGestureDetector("Circle", recordStream);
+                circleGestureRecognizer.TraceTo(gesturesCanvas, Colors.Red);
+                circleGestureRecognizer.OnGestureDetected += OnGestureDetected;
+            }
+            templates.ItemsSource = circleGestureRecognizer.LearningMachine.Paths;
+        }
+
+        void OnGestureDetected(string gesture)
+        {
+            int pos = detectedGestures.Items.Add(string.Format("{0} : {1}", gesture, DateTime.Now));
+
+            detectedGestures.SelectedIndex = pos;
+        }
+
+        void CloseGestureDetector()
+        {
+            if (circleGestureRecognizer == null)
+                return;
+
+            using (Stream recordStream = File.Create(circleKBPath))
+            {
+                circleGestureRecognizer.SaveState(recordStream);
+            }
+            circleGestureRecognizer.OnGestureDetected -= OnGestureDetected;
+        }
+    }
+}
---
+++ Experiments/Gestures/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Kinect.Toolbox;
+using Kinect.Toolbox.Record;
+using System.IO;
+using Microsoft.Kinect;
+using Microsoft.Win32;
+using Kinect.Toolbox.Voice;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow
+    {
+        KinectSensor kinectSensor;
+        SwipeGestureDetector swipeGestureRecognizer;
+        TemplatedGestureDetector circleGestureRecognizer;
+        readonly BarycenterHelper barycenterHelper = new BarycenterHelper();
+
+        string circleKBPath;
+
+        private Skeleton[] skeletons;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            circleKBPath = Path.Combine(Environment.CurrentDirectory, @"data\circleKB.save");
+
+            try
+            {
+                //loop through all the Kinects attached to this PC, and start the first that is connected without an error.
+                foreach (KinectSensor kinect in KinectSensor.KinectSensors)
+                {
+                    if (kinect.Status == KinectStatus.Connected)
+                    {
+                        kinectSensor = kinect;
+                        break;
+                    }
+                }
+
+                if (KinectSensor.KinectSensors.Count == 0)
+                    MessageBox.Show("No Kinect found");
+                else
+                    Initialize();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Initialize()
+        {
+            if (kinectSensor == null)
+                return;
+
+            kinectSensor.SkeletonStream.Enable(new TransformSmoothParameters
+            {
+                Smoothing = 0.5f,
+                Correction = 0.5f,
+                Prediction = 0.5f,
+                JitterRadius = 0.05f,
+                MaxDeviationRadius = 0.04f
+            });
+            kinectSensor.SkeletonFrameReady += kinectRuntime_SkeletonFrameReady;
+
+            swipeGestureRecognizer = new SwipeGestureDetector();
+            swipeGestureRecognizer.OnGestureDetected += OnGestureDetected;
+
+            kinectSensor.Start();
+
+            LoadCircleGestureDetector();
+        }
+
+        void kinectRuntime_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            {
+                if (frame == null)
+                    return;
+
+                Tools.GetSkeletons(frame, ref skeletons);
+
+                if (skeletons.All(s => s.TrackingState == SkeletonTrackingState.NotTracked))
+                    return;
+
+                ProcessFrame(frame);
+            }
+        }
+
+        void ProcessFrame(ReplaySkeletonFrame frame)
+        {
+            foreach (var skeleton in frame.Skeletons)
+            {
+                if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                    continue;
+
+                barycenterHelper.Add(skeleton.Position.ToVector3(), skeleton.TrackingId);
+                if (!barycenterHelper.IsStable(skeleton.TrackingId))
+                    return;
+
+                foreach (Joint joint in skeleton.Joints)
+                {
+                    if (joint.TrackingState != JointTrackingState.Tracked)
+                        continue;
+
+                    if (joint.JointType == JointType.HandRight)
+                    {
+                        swipeGestureRecognizer.Add(joint.Position, kinectSensor);
+                        circleGestureRecognizer.Add(joint.Position, kinectSensor);
+                    }
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Clean();
+        }
+
+        private void Clean()
+        {
+            if (swipeGestureRecognizer != null)
+            {
+                swipeGestureRecognizer.OnGestureDetected -= OnGestureDetected;
+            }
+
+            CloseGestureDetector();
+
+            if (kinectSensor != null)
+            {
+                kinectSensor.SkeletonFrameReady -= kinectRuntime_SkeletonFrameReady;
+                kinectSensor.Stop();
+                kinectSensor = null;
+            }
+        }
+    }
+}
