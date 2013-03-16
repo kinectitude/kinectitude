c86efd530e394177462f8ab2afa303a5449f9714
Brandon Walderman <bdwalder@uwaterloo.ca>
2013-03-16 15:22:46 -0400

    Quit action did not have a Plugin attribute and wasn't available for users.

--- Source/Kinectitude/Core/Actions/QuitAction.cs
+++ Source/Kinectitude/Core/Actions/QuitAction.cs
@@ -1,4 +1,5 @@
-﻿using Kinectitude.Core.Base;
+﻿using Kinectitude.Core.Attributes;
+using Kinectitude.Core.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using Action = Kinectitude.Core.Base.Action;
 
 namespace Kinectitude.Core.Actions
 {
+    [Plugin("quit game", "Quit game")]
     public class QuitAction : Action
     {
         public override void Run()
